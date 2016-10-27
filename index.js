44e0cb2261c0aefa24a7a1b824ffed7d2960b500
shunzizhan <shunzizhan@163.com>
2016-10-27 19:21:52 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,35 @@
+
+module.exports = function (content, file, opt) {
+  // 只对 html 类文件进行处理
+  // if (!file.isHtmlLike){
+  //   return content;
+  // }
+  // var newWords = opt.newWords,
+  //     ignoreCommentWords = opt.ignoreWords || [],
+  //     ignoreWordsReg = new RegExp(ignoreCommentWords.join('|'),'g');
+  // for(var ele in newWords){
+  //   var Reg = new RegExp(ele,'g');
+  //   content = content.replace(Reg,newWords[ele]);
+  // }
+  
+  // if(file.isHtmlLike){
+  //   // 删除注释
+  //   if(opt.removeComments){
+  //     content = content.replace(/(<!--)(.{0,})(-->)/g,function(match,g1,g2,g3,index,origin){
+  //       if(g2.search(ignoreWordsReg)>-1){
+  //         return g1+g2+g3;
+  //       }else{
+  //         return "";
+  //       }
+  //     });
+  //   }
+  //   // 压缩文件
+  //   if(opt.minifier){
+  //     content = content.replace(/>\s+</gm,"><");
+  //   }
+  // }
+  content = content.replace(/body(.)<html>(.)<\/html>(.)\/body/g,function(match,g1,g2,g3,index,origin){
+    return g1+g2+g3;
+  });
+  return content;
+};
